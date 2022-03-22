99c038a1ae03dc0ca3a6e5e47e5112c7a1ce2f58
ramkid91 <ramkid91@gmail.com>
2022-03-22 12:15:30 +0900

    feat:Add dataList and Set adpater, layoutManager for recyclerView

--- app/src/main/java/com/example/photos/MainActivity.kt
+++ app/src/main/java/com/example/photos/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.photos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        val randomColorList=  List<Int>(40){ColorFactory.makeRandomRGB().getColor()}
+        val adapter= PhotoAdapter(randomColorList)
+        recyclerView.adapter= adapter
+        recyclerView.layoutManager= GridLayoutManager(this, 4)
     }
 }
