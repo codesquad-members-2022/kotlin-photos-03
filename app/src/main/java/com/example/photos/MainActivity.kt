926bd9c62a18fe5e7f4539a2816fdeedda6390f8
ramkid91 <ramkid91@gmail.com>
2022-03-22 12:11:55 +0900

    feat: Add recyclerView layout

--- app/src/main/java/com/example/photos/MainActivity.kt
+++ app/src/main/java/com/example/photos/MainActivity.kt
@@ -2,11 +2,12 @@ package com.example.photos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
     }
 }
