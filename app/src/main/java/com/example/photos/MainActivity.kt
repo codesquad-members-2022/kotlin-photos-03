94354099a80599fe72db7697cf8bd50ed19de0db
DESKTOP-UIQTHKE\User <shn0185@naver.com>
2022-03-25 15:14:47 +0900

    http통신 json파일 받기

--- app/src/main/java/com/example/photos/MainActivity.kt
+++ app/src/main/java/com/example/photos/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.example.photos
 
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
-import android.widget.ImageView
 import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private val requestCodeImageGet = 100
@@ -30,31 +22,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val photoList = mutableListOf<Photo>()
-        findViewById<ImageButton>(R.id.ibtn_add_photo).setOnClickListener {
-            val photo = Photo(id, uri)
-            photoList.add(photo)
-        }
 
-        val adapter = PhotoAdapter(PhotoDiffCallback())
-        adapter.submitList(photoList)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = GridLayoutManager(this, 4)
+        val button = findViewById<ImageButton>(R.id.button)
 
-        activityResultLauncher =
-            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
-            if (it.resultCode == Activity.RESULT_OK) {
-                val uri = it.data?.data
-            }
-        }
+//        activityResultLauncher =
+//            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+//            if (it.resultCode == Activity.RESULT_OK) {
+//                val uri = it.data?.data
+//            }
+//        }
+//
+//        button.setOnClickListener{
+//            if (checkPermissions()) {
+//                val intent = Intent(Intent.ACTION_GET_CONTENT)
+//                intent.type = "image/*"
+//                activityResultLauncher.launch(intent)
+//            }
+//        }
 
-        button.setOnClickListener{
-            if (checkPermissions()) {
-                val intent = Intent(Intent.ACTION_GET_CONTENT)
-                intent.type = "image/*"
-                activityResultLauncher.launch(intent)
-            }
+        button.setOnClickListener {
+            val intent = Intent(this, StoreActivity::class.java)
+            startActivity(intent)
         }
     }
 
@@ -88,7 +76,11 @@ class MainActivity : AppCompatActivity() {
         }
 
         if (denyPermissions.isNotEmpty()) {
-            ActivityCompat.requestPermissions(this, denyPermissions.toTypedArray(), requestCodeImageGet)
+            ActivityCompat.requestPermissions(
+                this,
+                denyPermissions.toTypedArray(),
+                requestCodeImageGet
+            )
             return false
         }
         return true
